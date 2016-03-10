ebdd57f7c2963df0d23ab07778feab07c1392fce
Tianyu Lei <tianyule@usc.edu>
2016-03-10 14:42:21 -0800

    put downloaded files into new folder

--- csci571.py
+++ csci571.py
@@ -1,30 +1,24 @@
-from docx import Document
-from bs4 import BeautifulSoup
+import base64
 import requests
-from requests.auth import HTTPBasicAuth
-import urllib
+import urllib2
+from bs4 import BeautifulSoup
 import re
+import os
+newpath = os.getcwd()+'\\csci571_lecture'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
 USERNAME = 'csci571' # put correct usename here
 PASSWORD = 'notes1' # put correct password here
 url ="http://cs-server.usc.edu:45678/slides/"
-session = requests.session()
-
-req_headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
-
-formdata = {
-    'UserName': USERNAME,
-    'Password': PASSWORD,
-    'LoginButton' : 'Login'
-}
+base64string = base64.encodestring('%s:%s' % ("csci571", "notes1")).replace('\n', '')
 r = requests.get(url, auth=(USERNAME, PASSWORD))
-
 soup = BeautifulSoup(r.text)
 pdf_link =soup.findAll(name='a',attrs={'href':re.compile("pdf$")})
-out = pdf_link[0]["href"]
 for download_link in pdf_link:
     link = url+download_link["href"]
-    r = requests.get(url, auth=(USERNAME, PASSWORD))
-
-    urllib.urlretrieve (link, download_link["href"])
+    print link
+    request = urllib2.Request(link)
+    request.add_header("Authorization", "Basic %s" % base64string)
+    result = urllib2.urlopen(request)
+    file = open(newpath+"\\"+download_link["href"], 'wb')
+    file.write(result.read())
